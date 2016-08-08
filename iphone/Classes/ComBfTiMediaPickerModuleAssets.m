f64c89048085d38be21c989bdaf6f4a67dcf1e29
Duy Bao Nguyen <bduyng@gmail.com>
2016-08-08 16:38:54 +0700

    initial commit

---
+++ iphone/Classes/ComBfTiMediaPickerModuleAssets.m
@@ -0,0 +1,24 @@
+/**
+ * This is a generated file. Do not edit or your changes will be lost
+ */
+#import "ComBfTiMediaPickerModuleAssets.h"
+
+extern NSData* filterDataInRange(NSData* thedata, NSRange range);
+
+@implementation ComBfTiMediaPickerModuleAssets
+
+- (NSData*) moduleAsset
+{
+	
+
+	return nil;
+}
+
+- (NSData*) resolveModuleAsset:(NSString*)path
+{
+	
+
+	return nil;
+}
+
+@end
